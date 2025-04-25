0fa50efacc5a96eb753790ab92f2acd3e9a0c75a
Lucas Silva <lucas.silva@jassy.ag>
2025-04-25 13:28:49 -0300

    refactor: Change local .env, edit gitignore, included others paths

--- reflow-oven-ui/src/app/layout.tsx
+++ reflow-oven-ui/src/app/layout.tsx
@@ -1,6 +1,9 @@
 import { Metadata } from 'next';
 
 export function generateMetadata(): Metadata {
+  console.log('Generating metadata...');
+  console.log('NEXT_PUBLIC_APP_TITLE:', process.env.NEXT_PUBLIC_APP_TITLE);
+  console.log('NEXT_PUBLIC_APP_DESCRIPTION:', process.env.NEXT_PUBLIC_APP_DESCRIPTION); 
   return {
     title: process.env.NEXT_PUBLIC_APP_TITLE,
     description: process.env.NEXT_PUBLIC_APP_DESCRIPTION
