73e0d63b08467b99c839c82b297a0633a1ce3992
lucas_silva <lucasandrade730@gmail.com>
2025-04-27 16:21:40 -0300

    Included theme

--- reflow-oven-ui/src/app/layout.tsx
+++ reflow-oven-ui/src/app/layout.tsx
@@ -1,19 +1,44 @@
 import { Metadata } from 'next';
+import { Roboto } from 'next/font/google';
+import React from 'react';
+
+import CssBaseline from '@mui/material/CssBaseline';
+import { ThemeProvider } from '@mui/material/styles';
+import { AppRouterCacheProvider } from '@mui/material-nextjs/v15-appRouter';
+
+import { DarkTheme } from '@/shared/themes';
+import '../styles/globals.css';
 
 export function generateMetadata(): Metadata {
   console.log('Generating metadata...');
   console.log('NEXT_PUBLIC_APP_TITLE:', process.env.NEXT_PUBLIC_APP_TITLE);
-  console.log('NEXT_PUBLIC_APP_DESCRIPTION:', process.env.NEXT_PUBLIC_APP_DESCRIPTION); 
+  console.log('NEXT_PUBLIC_APP_DESCRIPTION:', process.env.NEXT_PUBLIC_APP_DESCRIPTION);
   return {
     title: process.env.NEXT_PUBLIC_APP_TITLE,
-    description: process.env.NEXT_PUBLIC_APP_DESCRIPTION
+    description: process.env.NEXT_PUBLIC_APP_DESCRIPTION,
   };
 }
 
+const roboto = Roboto({
+  weight: ['300', '400', '500', '700'],
+  subsets: ['latin'],
+  display: 'swap',
+  variable: '--font-roboto',
+});
+
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en">
-      <body>{children}</body>
+    <html lang="en" className={roboto.className}>
+      <AppRouterCacheProvider>
+        <body style={{ margin: 0, padding: 0 }}>
+          <ThemeProvider theme={DarkTheme}>
+            <React.Fragment>
+              <CssBaseline />
+              {children}
+            </React.Fragment>
+          </ThemeProvider>
+        </body>
+      </AppRouterCacheProvider>
     </html>
   );
 }
